41c9934a5c1f502facf622213ed4b12b33f7178f
Kuba Wichliński <kubawich45@gmail.com>
2018-08-28 12:11:25 +0200

    Manifest.json fixes

--- wwwroot/js/sw.js
+++ wwwroot/js/sw.js
@@ -1,32 +1,20 @@
-﻿if ('serviceWorker' in navigator) {
-    window.addEventListener('load', function () {
-        navigator.serviceWorker.register('./js/sw.js').then(function (registration) {
-            console.log('ServiceWorker registration successful with scope: ', registration.scope);
-        }, function (err) {
-            console.log('ServiceWorker registration failed: ', err);
-        });
-    });
-}
+﻿var CACHE_NAME = 'my-site-cache-v1';
 self.addEventListener('install', function (event) {
-    var CACHE_NAME = 'my-site-cache-v1';
-    var urlsToCache = [
-        '/',
-        '/css/site.min.css',
-        '/css/bootstrap.min.css',
-        '/js/site.min.js',
-        '/js/riot.min.js'
-    ];
-
-self.addEventListener('install', function (event) {
-     event.waitUntil(
-         caches.open(CACHE_NAME)
+    event.waitUntil(
+        caches.open(CACHE_NAME)
             .then(function (cache) {
                 console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-        );
+                return cache.addAll([
+                    '/',
+                    './css/site.min.css',
+                    './css/bootstrap.min.css',
+                    './js/site.min.js',
+                    './js/riot.min.js'
+                ]).then(function () {
+                    self.skipWaiting();
+                });
+        }));
     });
-});
 
 self.addEventListener('fetch', function (event) {
     event.respondWith(

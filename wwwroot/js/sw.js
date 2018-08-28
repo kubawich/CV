2d5a494316c278cf0acee58dd21ba741a62331a2
Kuba Wichliński <kubawich45@gmail.com>
2018-08-28 22:07:58 +0200

    Manifest & Service worker fixes

--- wwwroot/js/sw.js
+++ wwwroot/js/sw.js
@@ -1,60 +1,59 @@
-﻿var CACHE_NAME = 'my-site-cache-v1';
+﻿const CACHE_NAME = 'my-site-cache-v1';
+const RUNTIME = 'runtime';
 self.addEventListener('install', function (event) {
     event.waitUntil(
         caches.open(CACHE_NAME)
             .then(function (cache) {
                 console.log('Opened cache');
                 return cache.addAll([
-                    '/',
-                    './css/site.min.css',
-                    './css/bootstrap.min.css',
-                    './js/site.min.js',
-                    './js/riot.min.js'
+                    './',
+                    '../css/site.min.css',
+                    '../css/bootstrap.min.css',
+                    '../fonts/',
+                    '../CV.pdf',
+                    './site.min.js',
+                    './jquery.min.js',
+                    './materialize.min.js',
+                    './about.js',
+                    './blog.js',
+                    './portfolio.js',
+                    './riot.min.js'
                 ]).then(function () {
                     self.skipWaiting();
                 });
         }));
     });
 
-self.addEventListener('fetch', function (event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function (response) {
-                if (response) {
-                    return response;
-                }
-                var fetchRequest = event.request.clone();
-
-                return fetch(fetchRequest).then(
-                    function (response) {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-                        var responseToCache = response.clone();
-
-                        caches.open(CACHE_NAME)
-                            .then(function (cache) {
-                                cache.put(event.request, responseToCache);
-                            });
-
-                        return response;
-                    }
-                );
-            })
+self.addEventListener('activate', function (event) {
+    const currentCaches = [CACHE_NAME, RUNTIME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+        }).then(cachesToDelete => {
+            return Promise.all(cachesToDelete.map(cacheToDelete => {
+                return caches.delete(cacheToDelete);
+            }));
+        }).then(() => self.clients.claim())
     );
 });
 
-self.addEventListener('activate', function (event) {
-    var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
-    event.waitUntil(
-        caches.keys().then(function (cacheNames) {
-            return Promise.all(
-                cacheNames.map(function (cacheName) {
-                    if (cacheWhitelist.indexOf(cacheName) === -1) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
-        })
-    );
+
+self.addEventListener('fetch', event => {
+    if (event.request.url.startsWith(self.location.origin)) {
+        event.respondWith(
+            caches.match(event.request).then(cachedResponse => {
+                if (cachedResponse) {
+                    return cachedResponse;
+                }
+
+                return caches.open(RUNTIME).then(cache => {
+                    return fetch(event.request).then(response => {
+                        return cache.put(event.request, response.clone()).then(() => {
+                            return response;
+                        });
+                    });
+                });
+            })
+        );
+    }
 });

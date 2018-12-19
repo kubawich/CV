44b90eb406958cc1e6932bdaf464e4a9f52f8517
Kuba Wichliński <kubawich45@gmail.com>
2018-12-19 23:10:02 +0100

    Added game

--- wwwroot/js/blog.js
+++ wwwroot/js/blog.js
@@ -1,2 +1,2 @@
-riot.tag2('blog', '<div class="container row" id="Blog" style="height:200px;"> <h2 class="grey-text righ">Nothing here yet, come back later ;)</h2> </div>', '', '', function(opts) {
+riot.tag2('blog', '<div class="container row" id="Blog" style="height:200px;"> <article> <h3 id="blogTitle">Title</h3> <a href="/blog/1" id="blogTime"><p>18.02.20</p></a> <p id="blogContent">Artykuł here</p> </article> </div>', '', '', function(opts) {
 });
--- wwwroot/js/blog.tag
+++ wwwroot/js/blog.tag
@@ -1,6 +1,10 @@
 ﻿<blog>
   <div class="container row" id="Blog" style="height:200px;">
-    <h2 class="grey-text righ">Nothing here yet, come back later ;)</h2>
+		<article>
+			<h3 id="blogTitle">Title</h3>
+			<a href="/blog/1" id="blogTime"><p>18.02.20</p></a>
+			<p id="blogContent">Artykuł here</p>
+		</article>
   </div>
   <style>
     
